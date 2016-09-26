b0a9f55479f91d94fda57dfce8f2982b3735a86a
s0sbatuocs0s <doducquang1481996@gmail.com>
2016-09-26 08:15:23 +0700

    dsadasdasd

---
+++ baithuchanhso1/baithuchanhso1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baithuchanhso1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    class Vidu<A>
+     {
+         A Node;
+         private A Data;
+
+         public A Data
+         {
+             get { return Data; }
+             set { Data = value; }
+         }
+         Vidu<A> next;
+     }
+}
